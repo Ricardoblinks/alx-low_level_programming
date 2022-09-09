ad4cab9957f0d0a94b8ff8f88cdcee6537b35bc9
Ricardoblinks <108649095+Ricardoblinks@users.noreply.github.com>
2022-09-09 21:42:13 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,7 +11,8 @@ int main(void)
 
 {
 
-	int digit1 = 0;	int digit2, digit3;
+	int digit1 = 0;
+	int digit2, digit3;
 
 	while (digit1 <= 9)
 
@@ -50,7 +51,6 @@ int main(void)
 					{
 
 						putchar(',');
-
 						putchar(' ');
 
 					}
