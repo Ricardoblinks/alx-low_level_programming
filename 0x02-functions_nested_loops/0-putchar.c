303e663826bd7050a2a6df7a677acd9be23da74d
ricardoblinks <ricardoblinks1@gmail.com>
2022-09-13 17:36:53 +0100

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
+#include"main.h"
+
+/**
+ * main: entry point
+ * 
+ * return: always (0) success
+ */
 
 int main(void)
 {
-	printf("_putchar\n");
+	char array[] = "_putchar";
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(array[c]);
+	}
+	_putchar('\n');
+
 	return (0);
 }
