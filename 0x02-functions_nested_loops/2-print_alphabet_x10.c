0ce77760ba7ec3db578897e552b2cb2cb1fac273
ricardoblinks <ricardoblinks1@gmail.com>
2022-09-14 00:16:27 +0100

    starting to understand this

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10x to user
+ *
+ *description: print a - z 10 times on different lines
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		int alp;
+
+		for (alp = 'a'; alp <= 'z'; alp++)
+		{
+			_putchar(alp);
+		}
+
+		_putchar('\n');
+	}
