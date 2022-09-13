2e2747e7a6cd8fdfedf6c4aa0839fc8937c60914
ricardoblinks <ricardoblinks1@gmail.com>
2022-09-13 23:32:46 +0100

    Hot fix

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * main - entry point
+ *
+ * description: prints alphabets from a through z
+ * 
+ * return: always 0 for success
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+
+	return (0);
+}
