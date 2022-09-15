f89d5a5c8ec34d37f9e3275e80450b86065e1e41
ricardoblinks <ricardoblinks1@gmail.com>
2022-09-15 14:48:13 -0700

    Tough

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,27 @@
-#include"main.h"
-
+#include "main.h"
 /**
- * print_diagonal - print a diagonal line
- *
- * @n: is the number of times the \ character
- *     should be printed
-*/
-
+ *  * print_diagonal - prints diagonal stuff
+ *   *
+ *    * @n: integer to set diagonal
+ *     */
 void print_diagonal(int n)
 {
-	int postn, space;
-
-	if (n <= 0)
-		_putchar('\n');
-	else
-	{
-		for (postn = 1; postn <= n; ++postn)
-		{
-			for (space = 1; space <= postn; ++space)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
-			_putchar('\n');
-		}
-	}
+		int i;
+			int j;
+			
+				if (n <= 0)
+						{
+									_putchar('\n');
+										} else
+												{
+															for (i = 0; i < n; i++)
+																		{
+																						for (j = 0; j < i; j++)
+																										{
+																															_putchar(' ');
+																																		}
+																									_putchar('\\');
+																												_putchar('\n');
+																														}
+																}
 }
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,21 +1,26 @@
-#include"main.h"
-
+#include "main.h"
 /**
- * print_square - print a square using the character #
- *
- * @size: is the size of the square
- *
- * Return: Always 0 (Success)
-*/
-
+ *  * print_square - prints square stuff
+ *   *
+ *    * @size: integer to set square size
+ *     */
 void print_square(int size)
 {
-	int row, column;
+		int i;
+			int j;
 
-	for (row = 1; row <= size; ++row)
-	{
-		for (column = 1; column <= size; ++column)
-			_putchar('#');
-		_putchar('\n');
-	}
+				if (size <= 0)
+						{
+									_putchar('\n');
+										} else
+												{
+															for (i = 0; i < size; i++)
+																		{
+																						for (j = 0; j < size; j++)
+																										{
+																															_putchar('#');
+																																		}
+																									_putchar('\n');
+																											}
+																}
 }
