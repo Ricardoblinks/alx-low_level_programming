a653940f7b12ab3b9077ca3a9f78a5963fa7149b
Richard James <ricardoblinks1@gmail.com>
2022-10-03 11:36:23 -0400

    2nd task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,9 +6,13 @@
 char *_strdup(char *str)
 {
 	char *tmp;
-	tmp = str;
-
+	int i, j;
+	
 	if (str == NULL)
 		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		tmp[j] = str[j];
+	}
 	return (tmp);
 }
