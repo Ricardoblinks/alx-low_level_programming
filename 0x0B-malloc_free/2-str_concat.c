5d3cba2deb75343c4941d72f6a7773cbeab3ab8b
Richard James <ricardoblinks1@gmail.com>
2022-10-03 12:44:49 -0400

    3rd task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,16 +5,16 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *n;
 	int i, j;
 
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
-		n[i] = s1[i];
+		;
 
 	for (j = 0; s2[j] != '\0'; j++)
-		n[j] = s2[j];
+		i++;
+	s1[j] = s2[j];
 
-	return (n);
+	return (s1);
 }
